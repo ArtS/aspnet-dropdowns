1327448a4cb990254c887af771e37b9fcbd74128
Art Skvira <art@nimblegecko.com>
2014-10-06 22:06:20 +1100

    First project draft

---
+++ Dropdowns/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dropdowns.Models;
+
+namespace Dropdowns.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var states = new List<string>
+            {
+                "ACT",
+                "New South Wales",
+                "Northern Territories",
+                "Queensland",
+                "South Australia",                    
+                "Victoria",                    
+                "Western Australia",
+            };
+
+            var m = new SignUpModel
+            {
+                States = GetSelectListItems(states)
+            };
+
+            return View(m);
+        }
+
+        private IEnumerable<SelectListItem> GetSelectListItems(IEnumerable<string> elements) {
+            var result = new List<SelectListItem>();
+            foreach(var element in elements) 
+            {
+                result.Add(new SelectListItem 
+                {
+                    Value = element,
+                    Text = element
+                });
+            }
+
+            return result;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Dropdowns/Models/SignUpModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dropdowns.Models
+{
+    public class SignUpModel
+    {
+        public string State { get; set; }
+        public IEnumerable<SelectListItem> States { get; set; }
+    }
+}
